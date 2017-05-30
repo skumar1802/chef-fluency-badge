b1d9d343a3b29defd1335d4874bbcf8c164f2a32
skumar1802 <skumar1802@gmail.com>
2017-05-30 05:51:26 -0400

    new changes for node info

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node'platform_family'] --"debian"
+	package_name = "apache2"
+end
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
+
+include_recipe 'apache::websites'
